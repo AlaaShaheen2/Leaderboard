1550ad1c83eab2759ceb264006adae6f93750011
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-09-10 00:40:11 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './index.css';
 import Items from './modules/display';
 
-
 const refreshBtn = document.querySelector('.btn-ref');
 const name = document.querySelector('.name');
 const score = document.querySelector('.number');
 const form = document.querySelector('#form');
 
 const gameInfo = async (data) => {
-const resp = await fetch (`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RjH6JglhUiy5dVBUHC3M/scores
-`,{
+  const resp = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RjH6JglhUiy5dVBUHC3M/scores
+`, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -21,15 +20,15 @@ const resp = await fetch (`https://us-central1-js-capstone-backend.cloudfunction
 };
 
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const data = {
-        user: name.value,
-        score: score.value,
-      }
-    await gameInfo(data);
-    form.reset()
-  });
-  
-  refreshBtn.addEventListener('click', async () => {
-    await Items.takeScore();
-  });
+  e.preventDefault();
+  const data = {
+    user: name.value,
+    score: score.value,
+  };
+  await gameInfo(data);
+  form.reset();
+});
+
+refreshBtn.addEventListener('click', async () => {
+  await Items.takeScore();
+});
--- src/modules/gameFun.js
+++ src/modules/gameFun.js
@@ -1,19 +1,15 @@
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/scores', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: ' Highest Score',
+  }),
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((resp) => resp.json())
+  .then((data) => data);
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/scores',{
-    method: 'POST',
-    body: JSON.stringify({
-        name: ' Highest Score',
-    }),
-    headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-}
-)
-.then ((resp) => resp.json())
-.then((data) => data);
+const myId = 'RjH6JglhUiy5dVBUHC3M';
 
-const myId ='RjH6JglhUiy5dVBUHC3M';
-
-export  default myId;
-
-
+export default myId;
